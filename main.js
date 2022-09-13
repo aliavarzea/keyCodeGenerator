abc2e3092d10c4dfd169bd858504dbbd341cfc0d
Alia Varzea <aliavarzea@hotmail.com>
2022-09-13 16:00:41 +0100

    updates to html

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const displayKeyCode = document.querySelector('.keyCode h2');
 const keyCodeDiv = document.querySelector('.keyCode');
 const overlay = document.querySelector('.overlay');
 
-window.addEventListener('keydown',(e)=>{
-  overlay.classList.add('hide');
+window.addEventListener("keydown",(e)=>{
+  overlay.classList.add("hide");
   displayKey.innerHTML = e.key;
   displayKeyCode.innerHTML = e.keyCode;
   if(e.keyCode === 32){
-    displayKey.innerHTML = `'space'`
+    displayKey.innerHTML = `Space`
   }
 });
 
